0a2579559712eeb23aa2942348e1e02a4048912f
yamaso-burger <apple_1228_ringo@yahoo.co.jp>
2022-10-17 17:16:49 +0900

    add some statement to use npm package (body-parser, ejs)

--- app.js
+++ app.js
@@ -2,10 +2,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
 // mongoose
 const mongoose = require('mongoose');
+// ejs
+let ejs = require('ejs');
 
-mongoose.connect('mongodb://localhost:27017/wikiDB');
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect('mongodb://localhost:27017/wikiDB', {useNewUrlParser: true});
 
 const articleSchema = new mongoose.Schema({
     title: String,
@@ -33,11 +43,17 @@ function postArticle(title, content) {
 }
 
 // postArticle("ejs", "ejs is one of the npm module which makes creating web-pages easier")
-readAricles();
+// readAricles();
 
-app.get('/', (req, res)=>{
-    res.send('Hello');
-});
+app.get('/articles', function(req, res){
+    Article.find(function(err, foundArticles){
+        if (!err) {
+            res.send(foundArticles);
+        } else {
+            res.send(err);
+        }
+    });
+})
 
 app.listen(port, ()=>{
     console.log('running on port ' + port);
